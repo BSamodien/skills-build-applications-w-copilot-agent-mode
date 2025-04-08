b474a49f8dd349e540ad42d1de93b40c1cd87aa5
Brent <BSamodien@users.noreply.github.com>
2025-04-08 23:02:12 +0000

    Fix populate_db command and ensure test data population

---
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -0,0 +1,37 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'octofit_tracker_app',
+    'rest_framework',
+    'corsheaders',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'octofit_tracker',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
+    }
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
+ALLOWED_HOSTS = ['*']
+
+SITE_ID = 1
